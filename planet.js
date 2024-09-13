3b693010b68bbc87dd2e859ee253b8fdf3d38e62
tai <ledoantai2204@gmail.com>
2024-09-13 14:00:17 +0700

    Collision 2 planet same name

--- planet.js
+++ planet.js
@@ -28,15 +28,15 @@ let gravity = 9.81;
 let planetArray = [];
 
 const planets = [
-    { namePlanet: 1, size: 20, src: "./Assets/1.png", img: null },
-    { namePlanet: 2, size: 30, src: "./Assets/2.png", img: null },
-    { namePlanet: 3, size: 40, src: "./Assets/3.png", img: null },
-    { namePlanet: 4, size: 50, src: "./Assets/4.png", img: null },
-    { namePlanet: 5, size: 60, src: "./Assets/5.png", img: null },
-    { namePlanet: 6, size: 70, src: "./Assets/6.png", img: null },
-    { namePlanet: 7, size: 80, src: "./Assets/7.png", img: null },
-    { namePlanet: 8, size: 90, src: "./Assets/8.png", img: null },
-    { namePlanet: 9, size: 100, src: "./Assets/9.png", img: null },
+    { namePlanet: 0, size: 20, src: "./Assets/0.png", img: null },
+    { namePlanet: 1, size: 30, src: "./Assets/1.png", img: null },
+    { namePlanet: 2, size: 40, src: "./Assets/2.png", img: null },
+    { namePlanet: 3, size: 50, src: "./Assets/3.png", img: null },
+    { namePlanet: 4, size: 60, src: "./Assets/4.png", img: null },
+    { namePlanet: 5, size: 70, src: "./Assets/5.png", img: null },
+    { namePlanet: 6, size: 80, src: "./Assets/6.png", img: null },
+    { namePlanet: 7, size: 90, src: "./Assets/7.png", img: null },
+    { namePlanet: 8, size: 100, src: "./Assets/8.png", img: null },
 ];
 
 
@@ -73,10 +73,11 @@ function update() {
     context.stroke();
     
     if(!indexPlannet.spawned){
-        spawnPlannet(indexPlannet.x, indexPlannet.y);
+        setTimeout(spawnPlannet, 1000,indexPlannet.x, indexPlannet.y);
+        //spawnPlannet(indexPlannet.x, indexPlannet.y);
     }
 
-    planetArray.forEach(planet => {
+    planetArray.forEach((planet, i) => {
         if(planet.img && planet.img.complete){
             if(!planet.isDropped){
                 if(indexPlannet.x <= planet.radius/2){
@@ -101,8 +102,21 @@ function update() {
                 if(planet.x + planet.radius / 2 >= boardWidth) {
                     planet.x = boardWidth - planet.radius / 2; 
                 }
+
+                    for (let j = i + 1; j < planetArray.length; j++) {
+                        let otherPlanet = planetArray[j];
+                        if(!otherPlanet.isDropped){
+                            continue;
+                        }
+                        if (checkCollision(planet, otherPlanet)) {
+                            if(planet.namePlanet === otherPlanet.namePlanet){
+                                collisionSamePlanet(planet, otherPlanet);     
+
+                            }
+                        }
+                    }                
             }
-            context.drawImage(planet.img, planet.x - planet.radius/2, planet.y - planet.radius/2, planet.radius, planet.radius)
+            context.drawImage(planet.img, planet.x - planet.radius/2, planet.y - planet.radius/2, planet.radius, planet.radius);
         }
     });
 
@@ -135,7 +149,10 @@ function spawnPlannet(x,y){
         x : x,
         y : y,
         radius : planets[randomNumber].size,
-        isDropped : false
+        vx: 0,
+        vy: 0,
+        mass: planets[randomNumber].size / 10,
+        isDropped : false,
     };
 
     planetArray.push(planet);
@@ -143,6 +160,53 @@ function spawnPlannet(x,y){
 }
 
 function dropPlanet(){
-    planetArray[planetArray.length-1].isDropped = true;
+    for(let i = 0; i < planetArray.length; i++){
+        planetArray[i].isDropped = true;
+    }
+    
     indexPlannet.spawned = false;
+}
+
+function checkCollision(planet1, planet2) {
+    const dx = planet1.x - planet2.x;
+    const dy = planet1.y - planet2.y;
+    const distance = Math.sqrt(dx * dx + dy * dy);
+    return distance < (planet1.radius + planet2.radius) / 2;
+}
+
+function collisionSamePlanet(planet1, planet2){
+    let dx = planet2.x - planet1.x;
+    let dy = planet2.y - planet1.y;
+
+    let distance = Math.sqrt(dx * dx + dy * dy);
+
+    let nx = dx / distance;
+    let ny = dy / distance;
+
+    let collisionPointX = (planet1.x + nx * planet1.radius + planet2.x - nx * planet2.radius) / 2;
+    let collisionPointY = (planet1.y + ny * planet1.radius + planet2.y - ny * planet2.radius) / 2;
+
+    let newPlanet = planets[planet1.namePlanet + 1];
+    if(planet1.namePlanet + 1 >= planets.length)
+        newPlanet = planets[planet1.namePlanet];
+    let planet = {
+        namePlanet : newPlanet.namePlanet,
+        img : newPlanet.img,
+        x : collisionPointX,
+        y : collisionPointY,
+        radius : newPlanet.size,
+        vx: 0,
+        vy: 0,
+        mass: newPlanet.size / 10,
+        isDropped : true,
+        isCollision : false
+    };
+    const index1 = planetArray.indexOf(planet1);
+    const index2 = planetArray.indexOf(planet2);
+
+    if (index2 > -1) planetArray.splice(index2, 1);
+    if (index1 > -1) planetArray.splice(index1, 1);
+
+    planetArray.push(planet);
+    console.log(planetArray)
 }
